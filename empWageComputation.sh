b5dae530fbb8ed653298e0083b2608a8c00eeaa5
unknown <RockstarJay>
2021-06-03 13:58:47 +0530

    Employee Present or Absent Code Added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 echo "Welcome to Employee Wage Computation Program"
+
+isPresent=1
+checkemp=$((RANDOM%2))
+if [ $checkemp -eq $isPresent ]
+then
+	echo "Employee is Present"
+else
+	echo "Employee is Absent"
+fi
