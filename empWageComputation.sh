c2c6adb92ec688406684535b694e1ff39e24f987
unknown <RockstarJay>
2021-06-03 15:21:39 +0530

    Part Time Wage Computation Added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,28 +6,18 @@ isPartTime=1
 isFullTime=2
 empWagePerHour=20
 empHours=0
-monthlyWage=0
-count=0
-for (( i=0; i<20; i++ ))
-do
-	checkemp=$((RANDOM%3))
-	case $checkemp in
-		$isPartTime)
-			empHours=8
-			;;
-		$isFullTime)
-			empHours=16
-			;;
-		*)
-			empHours=0
-			;;
-	esac
-	dailyWage[$i]=$(($empHours*$empWagePerHour))
-done
-echo "Daily Employee Wage : ${dailyWage[@]}"
-for (( i=0; i<20; i++ ))
-do
-	wage=${dailyWage[i]}
-	monthlyWage=$((monthlyWage+wage))
-done
-echo " Employee's Monthly Wage is: " $monthlyWage
+checkemp=$((RANDOM%3))
+if [ $checkemp -eq $isPartTime ]
+then
+	echo "Employee Present Part Time"
+	empHours=8
+elif [ $checkemp -eq $isFullTime ]
+then
+	echo "Employee Present Full Time"
+	empHours=16
+else
+	echo "Employee is Absent"
+	empHours=0
+fi
+totalWage=$(($empHours*$empWagePerHour))
+echo "The Daily Wage is : " $totalWage
