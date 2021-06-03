33434a547f673705aaa1cf3a08d1d4c7de389d36
unknown <RockstarJay>
2021-06-03 15:30:51 +0530

    Monthly Wage till condition of total monthly hours or days is reached

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,19 +5,26 @@ echo "Welcome to Employee Wage Computation Program"
 isPartTime=1
 isFullTime=2
 empWagePerHour=20
-empHours=0
-checkemp=$((RANDOM%3))
-if [ $checkemp -eq $isPartTime ]
-then
-	echo "Employee Present Part Time"
-	empHours=8
-elif [ $checkemp -eq $isFullTime ]
-then
-	echo "Employee Present Full Time"
-	empHours=16
-else
-	echo "Employee is Absent"
-	empHours=0
-fi
-totalWage=$(($empHours*$empWagePerHour))
-echo "The Daily Wage is : " $totalWage
+monthlyHours=100
+monthlyWorkingDays=20
+monthlyWage=0
+workingHours=0
+for (( i=0; i<$monthlyWorkingDays && $workingHours<$monthlyHours; i++ ))
+do
+	checkemp=$((RANDOM%3))
+	case $checkemp in
+		$isPartTime)
+			empHours=8
+			;;
+		$isFullTime)
+			empHours=16
+			;;
+		*)
+			empHours=0
+			;;
+	esac
+	workingHours=$(($workingHours+$empHours))
+done
+monthlyWage=$(($workingHours*$empWagePerHour))
+echo " Employee's Monthly Wage is: " $monthlyWage
+
