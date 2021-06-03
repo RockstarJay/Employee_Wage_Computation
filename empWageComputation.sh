98a6c6568806ecdd835ae19e227ac3dbf4613973
unknown <RockstarJay>
2021-06-03 14:05:27 +0530

    Daily Wage Cpomutation Code Added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -7,6 +7,11 @@ checkemp=$((RANDOM%2))
 if [ $checkemp -eq $isPresent ]
 then
 	echo "Employee is Present"
+	empWagePerHour=20
+	empHours=8
+	DailyWage=$(($empHours*$empWagePerHour))
 else
 	echo "Employee is Absent"
+	DailyWage=0
 fi
+echo "Employee Daily Wage is : " $DailyWage
